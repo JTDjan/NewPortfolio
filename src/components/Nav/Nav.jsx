77216e7ed1ff1dbc5366caad42c708eab236c9e3
Your Name <you@example.com>
2022-03-13 01:06:26 +0000

    flex conatiners for nav & building hamburger icon

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -5,10 +5,22 @@ import "./Nav.scss";
 const Nav = () => {
   return (
     <nav className="nav">
-      Nav
+      <div className="wrapper">
+        <div className="left">
+          <a href="#landing" className="logo">
+            JTD Logo
+          </a>
+        </div>
+
+        <div className="right">
+          <div className="hamburger">
+            <span className="line1"></span>
+            <span className="line2"></span>
+            <span className="line3"></span>
+          </div>
+        </div>
+      </div>
     </nav>
-       
-     
   );
 };
 
