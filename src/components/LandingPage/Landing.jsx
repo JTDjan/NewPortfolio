b1c4bb604f20fabc4ab16c9e9b1d399c0844ecac
Your Name <you@example.com>
2022-04-09 18:24:31 +0100

    worked carried out on landing page

--- src/components/LandingPage/Landing.jsx
+++ src/components/LandingPage/Landing.jsx
@@ -1,12 +1,32 @@
-import React from 'react';
-import './landing.scss';
+import React from "react";
+import "./landing.scss";
 
 const Landing = () => {
   return (
-    <div className='landing' id='landing'>
-      Landing
+    <div className="landing" id="landing">
+      <div className="left">
+        <div className="imgContainer">
+          <img
+            src="Assets/Images/profilePic.jpeg"
+            alt="Profile pic of Josiah Djan, standing at the emirates stadium. COYG"
+          />
+        </div>
+      </div>
+      <div className="right">
+        <div className="wrapper">
+          <h2>Hi there, I'm</h2>
+          <h1>Josiah Djan</h1>
+          <h3>
+            Jnr Software developer <span>Designer</span>
+          </h3>
+          <a href="#about">
+            {/* <img src="" alt="" /> */}
+            down icon
+          </a>
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
 export default Landing;
